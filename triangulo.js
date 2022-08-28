98e9336bdd5358cdf8522425c9161ccc668b68b0
thierrytadashi <58242664+thierrytadashi@users.noreply.github.com>
2022-08-27 23:15:06 -0300

    Update triangulo.js

--- triangulo.js
+++ triangulo.js
@@ -2,12 +2,14 @@ function triangulo(var1, var2, var3) {
     var lado1 = var1;
     var lado2 = var2;
     var lado3 = var3;
-    if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2) {
+    if (lado1 < (lado2 + lado3) && lado2 < (lado1 + lado3 )&& lado3< (lado1 + lado2)) {
       if (lado1 == lado2 && lado2 == lado3) {
         return 'Equilatero';
-      } else if (lado1 == lado2 || lado1 == lado3 || lado3 == lado2) {
+      }
+        else if (lado1 == lado2 || lado1 == lado3 || lado3 == lado2) {
         return 'Isóceles';
-      } else {
+      } 
+        else {
         return 'Escaleno';
       }
     }
